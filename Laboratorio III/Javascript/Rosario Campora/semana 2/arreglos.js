7845420e28a5742f26ac3cbe4b9ebb06b611aa5d
RosarioCampora <mrosariocampora@gmail.com>
2023-06-15 00:18:07 +0200

    Javascript

---
+++ Laboratorio III/Javascript/Rosario Campora/semana 2/arreglos.js
@@ -0,0 +1,8 @@
+// La palabra continue
+for (let contado = 0; contado <= 10; contado++){
+    if(contado % 2 !== 0){
+        continue; // ir a la siguiente iteración
+    }
+    console.log(contado); //0, 2, 4, 6, 8, 10
+}
+console.log("Termina el ciclo"); //Termina el ciclo
---
+++ Laboratorio III/Javascript/Rosario Campora/semana 4/objetos.js
@@ -0,0 +1,38 @@
+
+//DIFERENTES FORMAS DE CREAR OBJETOS
+//caso numero 1
+let miObjeto = new Object(); //esta es una opcion formal
+
+//caso numero 2
+let miObjeto2 = {}; //esta opcion es breve y recomendada
+
+//caso numero 3 (String)
+let miCadena1 = new String('Ejemplo'); //Sintaxis formal
+
+//caso numero 4 (String 2)
+let miCadena2 = 'Hola'; //Estaes la sintaxis simplificada y recomendada
+
+//caso numero 5 (numeros 1)
+let miNumero = new Number(1); //es formal (no recomendada)
+
+//caso numero 6 (numeros 2)
+let miNumero2 = 1; //Sintaxis recomendada
+
+//caso numero 7 (boolean 1)
+let miBoolean1 = new Boolean(false); //formal
+
+//caso numero 8 (boolean2)
+let miBoolean2 = false; //sintaxis recomendada
+
+//caso numero 9 (arreglos 1)
+let miArrgelo = new Array(); //Formal
+
+//caso numero 10 (arreglos 2)
+let miArreglo2 = [0]; //Sintaxis recomendada
+
+
+//caso numero 11 (Funciones 1)
+let miFuncion1 = new function(){}; //Todo despues de new es considerado objeto
+
+//caso numero 12 (Funciones 2)
+let miFuncion2 = function(){}; //Notacion simplificada y recomendada
