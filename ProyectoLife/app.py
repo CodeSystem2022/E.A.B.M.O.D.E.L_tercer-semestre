ce721e57e6c92cb4e4d9a2a1ed63852abaa86c0d
Cristian Rosas <rosascristian26@gmail.com>
2023-06-13 22:26:55 -0300

    Arreglado home.py

--- ProyectoLife/app.py
+++ ProyectoLife/app.py
@@ -5,7 +5,7 @@ from  src.home import home
 app = Flask(__name__)
 
 
-app.secret_key = 'Emaesputo'
+app.secret_key = 'lautneslamejorilovepython'
 
 # Aca se registran los html / .py de cada uno
 app.register_blueprint(home)
