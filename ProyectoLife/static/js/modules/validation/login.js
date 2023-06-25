3f0ea6a5cddbf5f392c19400898f0589bd33bd07
Agustin Saravia <87948201+xNatzhu@users.noreply.github.com>
2023-06-25 19:21:52 -0300

    Modulo de validación login

---
+++ ProyectoLife/static/js/modules/validation/login.js
@@ -0,0 +1,10 @@
+import {Password, Email} from "./fields/fields.js"
+
+export default function ValidacionDeLogin() {
+    const form = document.getElementById("form-login");
+    
+    form.addEventListener("submit", (e) => {
+        Email(e)
+        Password(e)
+    });
+}
