cf4a8051fe6fd2a4d19d191202e8b987a9a123b0
chanta96 <gonzaloenzo.96@gmail.com>
2023-06-29 20:43:47 -0300

    base datos clase 11

---
+++ Programacion III/Java/Gonzalo Gramajo/clase 11/estudiantes.sql
@@ -0,0 +1,15 @@
+
+-- empezamos con crud create read update delete
+-- listar los estudiantes
+-- SELECT * FROM estudiantes;
+-- insertar estudiante
+-- INSERT INTO estudiantes(nombre, apellido, email) VALUES ("JUAN", "PEREZ", "GON@MAIL.COM");
+-- check db again
+-- SELECT * FROM estudiantes;
+-- update estudiante
+-- UPDATE estudiantes SET nombre ="gonzalo", apellido="gramajo" WHERE idestudiantes=1
+
+-- delete estudiante
+DELETE FROM estudiantes WHERE idestudiantes=5;
+
+SELECT * FROM estudiantes;
